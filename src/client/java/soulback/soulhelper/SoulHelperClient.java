f055063ba98f0c46d64f6099dbbb130a52292263
JustALostSoul <domlufi98@gmail.com>
2023-10-22 00:30:12 +0200

    Template Setup

---
+++ src/client/java/soulback/soulhelper/SoulHelperClient.java
@@ -0,0 +1,10 @@
+package soulback.soulhelper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SoulHelperClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
